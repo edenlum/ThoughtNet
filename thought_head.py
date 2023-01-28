1f803753e9efb8285dca7d91c0f7d3f20abb2f7e
edenlum <edenlum@gmail.com>
2023-01-28 16:13:41 +0200

    changing attn to self attention internal function

--- thought_head.py
+++ thought_head.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from copy import copy
+from model import MultiHeadedSelfAttention
 
 # Thought Head takes the output sequence of the transformer and returns another sequence of length input sequence + 1
 class ThoughtHead(nn.Module):
@@ -18,7 +19,7 @@ class ThoughtHead(nn.Module):
             nn.Linear(d_ff, dim),
             nn.Dropout(dropout)
         )
-        self.attn = nn.MultiheadAttention(dim, num_heads, dropout=dropout)
+        self.attn = MultiHeadedSelfAttention(dim, num_heads, dropout=dropout)
         self.class_token = nn.Parameter(torch.zeros(1, 1, dim))
         
     def forward(self, input):
@@ -27,7 +28,7 @@ class ThoughtHead(nn.Module):
         b, s, d = x.shape
         # b, s, d -> b, s+1, d
         x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
-        x = self.attn(x, x, x)
+        x = self.attn(x, None)
         x = self.ff(self.norm(x))
         x = torch.cat((x[:, 0], input[:, 1:]))
         return x
