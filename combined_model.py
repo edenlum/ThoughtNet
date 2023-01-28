d3bad35430e023e3e89585987f89ba76911564a6
edenlum <edenlum@gmail.com>
2023-01-28 15:35:59 +0200

    adding attention based thought head

--- combined_model.py
+++ combined_model.py
@@ -3,16 +3,24 @@ import torch.nn as nn
 
 # take a transformer model and convert it to a combined model
 class CombinedModel(nn.Module):
-    def __init__(self, transformer, thought_head, decision_head):
+    def __init__(self, preprocess, transformer, thought_head, decision_head):
         super().__init__()
-        self.transformer = transformer
-        self.thought_head = thought_head
+        self.preprocess = preprocess # patch embedding, class token, positional embedding
+        self.transformer = transformer 
+        self.thought_head = thought_head 
         self.decision_head = decision_head
 
     def forward(self, x, iters=0):
-        x = self.transformer(x)
+        """Preprocess, applies transformer + thought_head iters times, and then transformer one last time + decision head.
+        Args:
+            x (tensor): `b,c,fh,fw`
+        """
+        b, c, fh, fw = x.shape
+        x = self.preprocess(x)  # b,gh*gw+1,d 
+        x = self.transformer(x) # b, gh*gw+1, d
+        # every iteration, b, gh*gw+(i + 1), d -> b, gh*gw+(i + 2), d
         for i in range(iters):
             x = self.thought_head(x)
             x = self.transformer(x)
-        x = self.decision_head(x)
+        x = self.decision_head(x[:, 0]) # b, d -> b, num_classes
         return x
---
+++ thought_head.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from copy import copy
+
+# Thought Head takes the output sequence of the transformer and returns another sequence of length input sequence + 1
+class ThoughtHead(nn.Module):
+    def __init__(self, dim, d_ff, num_heads, dropout=0.1):
+        super().__init__()
+        self.d_model = dim
+        self.d_ff = d_ff
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.norm = nn.LayerNorm(dim)
+        self.ff = nn.Sequential(
+            nn.Linear(dim, d_ff),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, dim),
+            nn.Dropout(dropout)
+        )
+        self.attn = nn.MultiheadAttention(dim, num_heads, dropout=dropout)
+        self.class_token = nn.Parameter(torch.zeros(1, 1, dim))
+        
+    def forward(self, input):
+        # x is b, s, d
+        x = input
+        b, s, d = x.shape
+        # b, s, d -> b, s+1, d
+        x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
+        x = self.attn(x, x, x)
+        x = self.ff(self.norm(x))
+        x = torch.cat((x[:, 0], input[:, 1:]))
+        return x
