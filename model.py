3692b6c6966939da16e75a4c121e4b68a80c6b30
edenlum <edenlum@gmail.com>
2023-03-18 00:30:25 +0200

    fixed bug

--- model.py
+++ model.py
@@ -254,7 +254,10 @@ class ViT(nn.Module):
         x = self.patch_embedding(x)  # b,d,gh,gw
         x = x.flatten(2).transpose(1, 2)  # b,gh*gw,d
         orig_x = x
-        for _ in range(self.num_passes):
+        for i in range(self.num_passes):
+            if hasattr(self, 'next_token_predictor') and i > 0:
+                next_token = self.next_token_predictor(x[:, -1]).unsqueeze(1)  # b,d
+                x = torch.cat((orig_x, next_token), dim=1) # adding next token
             if hasattr(self, 'class_token'):
                 x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
             if hasattr(self, 'next_token_predictor'):
@@ -262,9 +265,7 @@ class ViT(nn.Module):
             if hasattr(self, 'positional_embedding'): 
                 x = self.positional_embedding(x)  # b,gh*gw+1,d 
             x = self.transformer(x)  # b,gh*gw+1,d
-            if hasattr(self, 'next_token_predictor'):
-                next_token = self.next_token_predictor(x[:, -1]).unsqueeze(1)  # b,d
-            x = torch.cat((orig_x, next_token), dim=1) # adding next token
+            
         if hasattr(self, 'pre_logits'):
             x = self.pre_logits(x)
             x = torch.tanh(x)
